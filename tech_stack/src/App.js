4d790724ebfd215a64740760341180550fe5cab4
Thirumal Govindaraj <javathirumalg@gmail.com>
2017-11-23 11:50:46 +0530

    Library list sample

---
+++ tech_stack/src/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {  
+  View
+} from 'react-native';
+import {Header} from './components/common';
+import LibraryList from './components/LibraryList';
+
+
+ const App = () => {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tach Stack"/>
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+};
+
+export default App;
+
